29620c2e98d2876f1834d0907ed51901409e79d5
Kovács Anna <anna.kovacs@stud.uni-obuda.hu>
2022-04-26 21:19:22 +0200

    .

---
+++ HGOBUC_HFT_2021222.Models/Actors.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HGOBUC_HFT_2021222.Models
+{
+    public class Actors
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        private int actorID { get; set; }  
+
+        private string ActorName { get; set; }
+
+    }
+}
