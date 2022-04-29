7344cb33a6c3ce28a0339f6616ae696b12beca0a
Kovács Anna <anna.kovacs@stud.uni-obuda.hu>
2022-04-29 11:31:20 +0200

    DbConext

---
+++ HGOBUC_HFT_2021222.Repository/MovieDbContext.cs
@@ -0,0 +1,34 @@
+﻿using HGOBUC_HFT_2021222.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace HGOBUC_HFT_2021222.Repository
+{
+    public class MovieDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actors> Actors { get; set; }
+        public DbSet<Network> Networks { get; set; }
+
+        public MovieDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (!builder.IsConfigured)
+            {
+                builder
+                   .UseLazyLoadingProxies()
+                   .UseInMemoryDatabase("movie");
+                ;
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+        }
+    }
+}
