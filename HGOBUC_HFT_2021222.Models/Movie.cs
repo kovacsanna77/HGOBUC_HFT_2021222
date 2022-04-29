8695183fbca095c5cb53d441bfaa4f3567cf0575
Kovács Anna <anna.kovacs@stud.uni-obuda.hu>
2022-04-29 10:38:24 +0200

    Model classes

---
+++ HGOBUC_HFT_2021222.Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HGOBUC_HFT_2021222.Models
+{
+    public class Movie
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public int Aired { get; set; }
+        public string Network { get; set; }
+        public int Episodes { get; set; }
+        public int Duration { get; set; }
+        public virtual ICollection<Actors> Actors { get; set; }
+
+    }
+}
---
+++ HGOBUC_HFT_2021222.Models/Network.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HGOBUC_HFT_2021222.Models
+{
+   public  class Network
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int NetworkId { get; set; }
+        public string NetworkName { get;   set; }
+        public virtual ICollection<Movie> Movies{ get; set; }
+    }
+}
