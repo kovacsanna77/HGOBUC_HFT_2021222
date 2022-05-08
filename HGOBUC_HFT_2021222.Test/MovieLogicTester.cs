6c264d2eb88012f0ae46d2be7fc0332789e89742
Kovács Anna <anna.kovacs@stud.uni-obuda.hu>
2022-05-08 19:45:57 +0200

    Test

---
+++ HGOBUC_HFT_2021222.Test/MovieLogicTester.cs
@@ -0,0 +1,23 @@
+﻿using HGOBUC_HFT_2021222.Logic.Classes;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HGOBUC_HFT_2021222.Test
+{
+    [TestFixture]
+    public class MovieLogicTester
+    {
+        MovieLogic logic;
+
+        [SetUp]
+        public void Init()
+        {
+
+        } 
+
+    }
+}
