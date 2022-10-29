f45179e17a686fbec413945d5c1f5c6d1abfbc50
kovacsanna77 <anna.kovacs@stud.uni-obuda.hu>
2022-10-29 10:41:51 +0200

    MoviesUI

--- HGOBUC_HFT_2021222.JSClient/wwwroot/script.js
+++ HGOBUC_HFT_2021222.JSClient/wwwroot/script.js
@@ -1,3 +1,18 @@
-﻿fetch('http://localhost:27826/Movie')
+﻿let movies = [];
+
+fetch('http://localhost:27826/Movie')
     .then(x => x.json())
-    .then(y => console.log(y));
+    .then(y => {
+        movies = y;
+        console.log(movies);
+        display();
+    });
+
+function display() {
+    movies.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.movieId + "</td><td>" + t.title + "</td><td>" + t.aired + "</td><td>"
+            + t.episodes + "</td><td>" + t.duration + "</td><td>" + t.network.networkName + "</td><td>" + t.rating;
+        
+    })
+}
