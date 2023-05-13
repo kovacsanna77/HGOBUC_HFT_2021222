b12a125aeb8d7a98ffb5ba177cb7e73f5a299ee8
Kovács Anna <anna.kovacs@stud.uni-obuda.hu>
2023-05-13 21:37:02 +0200

    JSClient létrehozva

--- HGOBUC_HFT_2021222.JSClient/wwwroot/script.js
+++ HGOBUC_HFT_2021222.JSClient/wwwroot/script.js
@@ -1,18 +1,1 @@
-﻿let movies = [];
-
-fetch('http://localhost:27826/Movie')
-    .then(x => x.json())
-    .then(y => {
-        movies = y;
-        console.log(movies);
-        display();
-    });
-
-function display() {
-    movies.forEach(t => {
-        document.getElementById('resultarea').innerHTML +=
-            "<tr><td>" + t.movieId + "</td><td>" + t.title + "</td><td>" + t.aired + "</td><td>"
-            + t.episodes + "</td><td>" + t.duration + "</td><td>" + t.network.networkName + "</td><td>" + t.rating;
-        
-    })
-}
+﻿
