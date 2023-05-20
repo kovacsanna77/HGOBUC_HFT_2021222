f4274c7d084a2d66423c2138b6a644020737954a
Kovács Anna <anna.kovacs@stud.uni-obuda.hu>
2023-05-20 17:34:34 +0200

    Js done, delete and create needs fix

---
+++ HGOBUC_HFT_2021222.JSClient/wwwroot/roleScript.js
@@ -0,0 +1,89 @@
+﻿let roles = [];
+let connection = null;
+getdata();
+setupSignalR();
+
+
+function setupSignalR() {
+    connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:27826/hub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    connection.on("RoleCreated", (user, message) => {
+        getdata();
+    });
+
+    connection.on("RoleDeleted", (user, message) => {
+        getdata();
+    });
+
+    connection.onclose(async () => {
+        await start();
+    });
+    start();
+
+
+}
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
+async function getdata() {
+    await fetch('http://localhost:27826/Role')
+        .then(x => x.json())
+        .then(y => {
+            networks = y;
+            display();
+        });
+}
+
+function display() {
+    document.getElementById('resultarea').innerHTML += '';
+    networks.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.roleId + "</td>"
+        + "<td>" + t.roleName + "</td>"
+        + "<td>" + t.actor.actorName + "</td><td>"
+            + `<button type="button" onclick="remove(${t.roleId})">Delete</button>`
+            + "</td></tr>";
+    });
+}
+
+function remove(id) {
+    fetch('http://localhost:27826/Role' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+
+}
+
+function create() {
+    let name = document.getElementById('rolename').value;
+    fetch('http://localhost:27826/Role', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { roleName: name })
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+}
--- HGOBUC_HFT_2021222.JSClient/wwwroot/script.js
+++ HGOBUC_HFT_2021222.JSClient/wwwroot/script.js
@@ -1,60 +1,112 @@
-﻿let movies = [];
+﻿let stats = [];
+let a10 = [];
+let a5m = [];
+let m10 = [];
+let avgratingbynetwork = [];
+let avgeppernetwork = [];
 
-getdata();
+Actorswith10rated();
+Actorswith5ratingandmovie();
+Movieswith10rating();
+AvgRatingByNetwork();
+AvgEpisodesPerNetwork()
 
-async function getdata() {
-   await fetch('http://localhost:27826/Movie')
-            .then(x => x.json())
-            .then(y => {
-                movies = y;
-                //console.log(movies);
-                display();
-          });
+function Actorswith5ratingandmovie() {
+    fetch('http://localhost:27826/Stat/ActorWith5RatedMovie')
+        .then(x => x.json())
+        .then(y => {
+            a5m = y;
+            console.log(a5m);
+            displaya5m();
+        }
+        );
+
+} function displaya5m() {
+    a5m.forEach(t => {
+        document.getElementById('actorswith5').innerHTML +=
+            "<tr><td>" + t.key + "</td><td>" + t.value + "</td></tr>";
+    })
 }
 
-function display() {
-    document.getElementById('resultarea').innerHTML += "";
-    movies.forEach(t => {
-        document.getElementById('resultarea').innerHTML +=
-          "<tr><td>" + t.movieId + "</td>"
-            + "<td>" + t.title + "</td>"   
-            + "<td>" + t.aired + "</td>"
-            + "<td>" + t.episodes + "</td>"
-            + "<td>" + t.duration + " min" + "</td>"
-            + "<td>" + t.rating + "</td>"
-        + "<td>" + t.network.networkName + "</td><td>"  + `<button type="button" onclick="remove(${t.movieId})">Delete</button>`
-        + "</td></tr>";
+
+
+function AvgRatingByNetwork() {
+    fetch('http://localhost:27826/Stat/AvgMovieRateByNetwork')
+        .then(x => x.json())
+        .then(y => {
+            avgratingbynetwork = y;
+            console.log(avgratingbynetwork);
+           displayavgratingbynetw();
+        }
+        );
+}
+
+function displayavgratingbynetw() {
+    avgratingbynetwork.forEach(t => {
+        document.getElementById('avgratingnetwork').innerHTML +=
+            "<tr><td>" + t.key + "</td><td>" + t.value + "</td></tr>";
+    })
+}
+
+
+
+
+function AvgEpisodesPerNetwork() {
+    fetch('http://localhost:27826/Stat/AvgEpisodesPerNetwork')
+        .then(x => x.json())
+        .then(y => {
+            avgeppernetwork = y;
+            console.log(avgeppernetwork);
+            displayAvgEp();
+        }
+        );
+
+} function displayAvgEp() {
+    avgeppernetwork.forEach(t => {
+        document.getElementById('avgEpPerNetwork').innerHTML +=
+            "<tr><td>" + t.key + "</td><td>" + t.value + "</td></tr>";
+    })
+}
+
+
+
+
+
+function Movieswith10rating() {
+    fetch('http://localhost:27826/Stat/MoviesWith10Rating')
+        .then(x => x.json())
+        .then(y => {
+           m10 = y;
+            console.log(m10);
+            displaym10();
+        }
+        );
+}
+function displaym10() {
+    m10.forEach(t => {
+        document.getElementById('movies10').innerHTML +=
+            "<tr><td>" + t.key + "</td><td>" + t.value + "</td></tr>";
+    })
+
+}
+
+
+function displaya10() {
+   
+    a10.forEach(t => {
+        document.getElementById('actorswith10').innerHTML +=
+            "<tr><td>" + t + "</td></tr>"
+            
     });
 }
 
-function remove(id) {
-    fetch('http://localhost:27826/Movie' + id, {
-        method: 'DELETE',
-        headers: { 'Content-Type': 'application/json', },
-        body: null
-    })
-        .then(response => response)
-        .then(data => {
-            console.log('Success:', data);
-            getdata();
-        })
-        .catch((error) => { console.error('Error:', error); });
+function Actorswith10rated() {
+    fetch('http://localhost:27826/Stat/ActorsWith10Rating')
+        .then(x => x.json())
+        .then(y => {
+            a10 = y;
+            console.log(a10);
+            displaya10();
 }
-
-function create() {
-    let name = document.getElementById('moviename').value;
-    fetch('http://localhost:27826/Movie', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json', },
-        body: JSON.stringify(
-            { title: name })
-    })
-        .then(response => response)
-        .then(data => {
-            console.log('Success:', data);
-            getdata();
-        })
-        .catch((error) => { console.error('Error:', error); });
-    
-    
+    );
 }
